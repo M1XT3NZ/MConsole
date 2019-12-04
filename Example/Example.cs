5e689102d60ed6913138ec6bd011c017cd0fa578
M1XT3NZ <glpfan90@gmail.com>
2019-12-04 20:21:24 +0100

    Some Documentation in the Example Project was added

--- Example/Example.cs
+++ Example/Example.cs
@@ -16,20 +16,20 @@ namespace Test
             InitializeComponent();
         }
 
-        SConsole.MConsole mconsole = new SConsole.MConsole();
+        SConsole.MConsole mconsole = new SConsole.MConsole();//define the console
         private void Form1_Load(object sender, EventArgs e)
         {
-            mconsole.Init("The Mighty Console");
-            mconsole.writetofile = true;
+            mconsole.Init("The Mighty Console");//This initiates the Console window this is not needed if writetofile is true
+            mconsole.writetofile = true; //If its true it will write to a file which exists in the program folder
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            mconsole.DEBUGMSG("I'M A DEFAULT COLORED DEBUGMSG");
-            mconsole.DEBUGMSG("I'M AN AQUA COLORED DEBUGMSG",Color.Aqua);
-            //Thread.Sleep(600);
-            mconsole.LOG("I'M THE DEFAULT COLOR");
-            mconsole.LOG("I'M A DARKGOLDENROD COLOR",Color.DarkGoldenrod);
+            mconsole.DEBUGMSG("I'M A DEFAULT COLORED DEBUGMSG");//Default Debugmessage log
+            mconsole.DEBUGMSG("I'M AN AQUA COLORED DEBUGMSG",Color.Aqua);//Aqua colored Debugmessage log
+
+            mconsole.LOG("I'M THE DEFAULT COLOR");//Default Log messasge
+            mconsole.LOG("I'M A DARKGOLDENROD COLOR",Color.DarkGoldenrod);//DarkGoldenrod log message
         }
     }
 }
